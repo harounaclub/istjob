61e681e3a94d32612b9b705fb9ba6a0c3335a0e9
System Admin <finasystechno@gmail.com>
2018-10-10 08:31:01 +0000

    mettre à jour les infos du contacts

--- application/modules/corporate/views/que_faisons_nous_view.php
+++ application/modules/corporate/views/que_faisons_nous_view.php
@@ -89,17 +89,7 @@
 
    <!-- Blue Area -->
     <div class="container-fluid blue-banner">
-                <div class="row">
-                    <div class="container main-container">
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ">
-                            <h3 class="white-heading">Avez vous une question ?</h3>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12"><span class="call-us">envoyer nous un email or <strong>appeler nous au 1 (800) 555-5555</strong></span></div>
-                        <div class="col-lg-2 col-md-4 col-sm-12 col-xs-12">
-                            <a href="nous_contacter" class="btn btn-getstarted bg-red">Nous contacter</a>
-                        </div>
-                    </div>
-                </div>
+                 <?php include("templates/avezvous_question.php"); ?>
             </div>
    
   
